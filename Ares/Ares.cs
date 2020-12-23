e457c462cc65f864131f77e78a700a49d8f14de9
KomradeSpectre <jchurch093@gmail.com>
2020-12-23 00:03:00 -0500

    Ares pre-version 0.0.1 release, nothing implemented yet.

---
+++ Ares/Ares.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using R2API;
+using R2API.Networking;
+using R2API.Utils;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Ares
+{
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(BuffAPI), nameof(LanguageAPI), nameof(ResourcesAPI),
+                          nameof(PlayerAPI), nameof(PrefabAPI), nameof(SoundAPI), nameof(OrbAPI),
+                          nameof(NetworkingAPI), nameof(EffectAPI), nameof(EliteAPI))]
+    public class Ares : BaseUnityPlugin
+    {
+        public const string ModVer = "0.0.1";
+        public const string ModName = "Ares";
+        public const string ModGuid = "com.KomradeSpectre.Ares";
+
+        private void Awake()
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Ares.ares_assets"))
+            {
+                var bundle = AssetBundle.LoadFromStream(stream);
+                var provider = new AssetBundleResourcesProvider("@Ares", bundle);
+                ResourcesAPI.AddProvider(provider);
+            }
+        }
+    }
+}
---
+++ Ares/Elite Equipment/AffixBase.cs
@@ -0,0 +1,105 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using System;
+using UnityEngine;
+
+namespace Ares.Elite_Equipment
+{
+    public abstract class AffixBase<T> : AffixBase where T : AffixBase<T>
+    {
+        public static T instance { get; private set; }
+
+        public AffixBase()
+        {
+            if (instance != null) throw new InvalidOperationException("Singleton class \"" + typeof(T).Name + "\" inheriting EquipmentBoilerplate/Equipment was instantiated twice");
+            instance = this as T;
+        }
+    }
+
+    public abstract class AffixBase
+    {
+        // Equipment Generation Settings
+
+
+        // Strings
+        public string BaseTypeNamePrefix = "AFFIX_";
+        public abstract string AffixEquipmentName { get; }
+        public abstract string AffixBuffName { get; }
+        public abstract string AffixLangTokenName { get; }
+        public abstract string AffixPickupDesc { get; }
+        public abstract string AffixFullDescription { get; }
+        public abstract string AffixLore { get; }
+
+        public abstract string AffixModelPath { get; }
+        public abstract string AffixIconPath { get; }
+
+        public virtual bool AppearsInSinglePlayer { get; } = true;
+
+        public virtual bool AppearsInMultiPlayer { get; } = true;
+
+        public virtual bool CanDrop { get; } = true;
+
+        public virtual float Cooldown { get; } = 60f;
+
+        public virtual bool EnigmaCompatible { get; } = true;
+
+        public virtual bool IsBoss { get; } = false;
+
+        public virtual bool IsLunar { get; } = false;
+
+        public EquipmentIndex AffixIndex;
+
+        public abstract void Init(ConfigFile config);
+
+        public abstract ItemDisplayRuleDict CreateItemDisplayRules();
+
+        protected void CreateLang()
+        {
+            LanguageAPI.Add(BaseTypeNamePrefix + AffixLangTokenName + "_NAME", AffixName);
+            LanguageAPI.Add(BaseTypeNamePrefix + AffixLangTokenName + "_PICKUP", AffixPickupDesc);
+            LanguageAPI.Add(BaseTypeNamePrefix + AffixLangTokenName + "_DESCRIPTION", AffixFullDescription);
+            LanguageAPI.Add(BaseTypeNamePrefix + AffixLangTokenName + "_LORE", AffixLore);
+        }
+
+        protected void CreateAffix()
+        {
+            EquipmentDef affixDef = new RoR2.EquipmentDef()
+            {
+                name = BaseTypeNamePrefix + AffixLangTokenName,
+                nameToken = BaseTypeNamePrefix + AffixLangTokenName + "_NAME",
+                pickupToken = BaseTypeNamePrefix + AffixLangTokenName + "_PICKUP",
+                descriptionToken = BaseTypeNamePrefix + AffixLangTokenName + "_DESCRIPTION",
+                loreToken = BaseTypeNamePrefix + AffixLangTokenName + "_LORE",
+                pickupModelPath = AffixModelPath,
+                pickupIconPath = AffixIconPath,
+                appearsInSinglePlayer = AppearsInSinglePlayer,
+                appearsInMultiPlayer = AppearsInMultiPlayer,
+                canDrop = CanDrop,
+                cooldown = Cooldown,
+                enigmaCompatible = EnigmaCompatible,
+                isBoss = IsBoss,
+                isLunar = IsLunar
+            };
+            var itemDisplayRules = CreateItemDisplayRules();
+            AffixIndex = ItemAPI.Add(new CustomEquipment(affixDef, itemDisplayRules));
+            On.RoR2.EquipmentSlot.PerformEquipmentAction += PerformEquipmentAction;
+        }
+
+        private bool PerformEquipmentAction(On.RoR2.EquipmentSlot.orig_PerformEquipmentAction orig, RoR2.EquipmentSlot self, EquipmentIndex equipmentIndex)
+        {
+            if (equipmentIndex == AffixIndex)
+            {
+                return ActivateAffix(self);
+            }
+            else
+            {
+                return orig(self, equipmentIndex);
+            }
+        }
+
+        protected abstract bool ActivateAffix(EquipmentSlot slot);
+
+        public abstract void Hooks();
+    }
+}
